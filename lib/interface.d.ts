dcf72c7ff48f672ae8e46e4098c5ef44803091bb
M Fadli Gunardi <68526207+mfadl1@users.noreply.github.com>
2023-09-14 08:50:59 +0700

    Delete lib directory

--- lib/interface.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ProductCrudService {
-    getProducts(agripointId?: number, productId?: number, isActive?: boolean): Promise<Iterable<any>>;
-}
-//# sourceMappingURL=interface.d.ts.map
